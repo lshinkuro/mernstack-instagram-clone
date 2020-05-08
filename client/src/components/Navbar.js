37d16fd931f939709ccb5ce5883709a477a10e0e
lshinkuro <kholis478@gmail.com>
2020-05-08 16:06:23 +0700

    add fix link forget about it

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,16 +1,29 @@
-import React from 'react'
+import React,{useContext}from 'react'
+import {Link}from 'react-router-dom'
+import {UserContext}from '../App'
 
 
 const Navbar = ()=>{
+    const {state,dispatch}=useContext(UserContext)
+    const renderList = ()=>{
+      if(state){
+        return [
+          <li><Link to="/profile">Profile</Link></li>,
+          <li><Link to="/createpost">CreatePost</Link></li>
+        ]
+      }else{
+        return [
+          <li><Link to="/signin">Login</Link></li>,
+          <li><Link to="/signup">Signup</Link></li>
+        ]
+      }
+    }
     return(
       <nav>
         <div className="nav-wrapper white">
-        <a href="/home" className="brand-logo left">INSTAGRAM</a>
-          <ul id="nav-mobile" className="right hide-on-med-and-down">
-            <li><a href="/signin">Login</a></li>
-            <li><a href="/signup">Signup</a></li>
-            <li><a href="/profile">Profile</a></li>
-            <li><a href="/createpost">CreatePost</a></li>
+        <Link href={state?"/":"/signin"} className="brand-logo left">INSTAGRAM</Link>
+          <ul id="nav-mobile" className="right">
+              {renderList()}
           </ul>
         </div>
       </nav>
